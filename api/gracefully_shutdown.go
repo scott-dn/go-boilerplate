18c801d460dc479d0307bc64db90e34199b39026
Scott Dn <scott.dn91@gmail.com>
2023-09-02 02:50:25 +0700

    refactor graceful shutdown

--- api/gracefully_shutdown.go
+++ api/gracefully_shutdown.go
@@ -15,13 +15,17 @@ import (
 func gracefullyShutdown(app *app.App, server *echo.Echo, hc *healthcheck) {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
 	log.Info().Str("signal", (<-quit).String()).Msg("received signal")
-	hc.Close()
+
+	hc.close()
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer func() {
 		app.Close()
 		cancel()
 	}()
+
 	log.Info().Msg("shutting down http server")
 	if err := server.Shutdown(ctx); err != nil {
 		log.Panic().Err(err).Msg("failed to shutdown http server")
